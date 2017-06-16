b109e7c957c35db8eab6639e0f8bf0dfde8bd301
Sean JONES <sjones@e1z2r5p43.42.us.org>
2017-06-15 19:25:14 -0700

    bloop

--- src/init_db.c
+++ src/init_db.c
@@ -6,7 +6,7 @@
 /*   By: sjones <sjones@student.42.us.org>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/05 15:48:24 by sjones            #+#    #+#             */
-/*   Updated: 2017/06/15 18:55:17 by sjones           ###   ########.fr       */
+/*   Updated: 2017/06/15 19:25:02 by sjones           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ t_db	*init_db(FILE *fp, int ac, char **av)
 
 	if (!(db = ft_memalloc(sizeof(t_db))))
 		return (NULL);
-	k = 0;
+	k = 1;
     line = "";
 	db->ents = NULL;
     get_next_line(fileno(fp), &tmp);
