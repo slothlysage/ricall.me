c6a1281f0f7c105ed15eebd85d001f374410044c
Sean JONES <sjones@e1z2r5p43.42.us.org>
2017-06-15 21:48:24 -0700

    input_file debugging

--- src/input_file.c
+++ src/input_file.c
@@ -6,7 +6,7 @@
 /*   By: sjones <sjones@student.42.us.org>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/05 15:48:24 by sjones            #+#    #+#             */
-/*   Updated: 2017/06/15 21:42:39 by sjones           ###   ########.fr       */
+/*   Updated: 2017/06/15 21:43:52 by sjones           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,38 +19,24 @@ int		input_file(t_db *db)
 	char	*line;
 	int		k;
 
-	ft_putendl("am I here?1");
 	fp = fopen("output_file.json", "r+");
-	ft_putendl("am I here after I opened file?");
 	k = 1;
     line = "";
     get_next_line(fileno(fp), &tmp);
-	ft_putendl("am I here after a GNL?");
-	ft_putendl(tmp);
 	while (get_next_line(fileno(fp), &tmp) > 0)
 	{
-		ft_putendl("am I here loopin?");
 		if (k % 17 == 1)
-		{
-			ft_putendl("hit an event loopin?");
             db->entry_count += 1;
-		}
 		else if (k % 17 == 0)
 		{
-			ft_putendl("last line loopin?");
 			line = ft_strjoin(line, tmp);
-			ft_putendl(line);
 			add_ent(db, line);
 			line = "";
 		}
 		else
-		{
-			ft_putendl("getting data loopin?");
 			line = ft_strjoin(line, tmp);
-		}
 		k += 1;
 	}
 	fclose(fp);
-	ft_putendl("I survived file input! :)");
 	return (1);
 }
