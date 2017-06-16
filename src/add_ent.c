b6eeb594eaf4e0f9f9859a4262f0c655c8c58f20
Sean JONES <sjones@e1z2r5p43.42.us.org>
2017-06-15 18:37:24 -0700

    more debug

--- src/add_ent.c
+++ src/add_ent.c
@@ -6,7 +6,7 @@
 /*   By: sjones <sjones@student.42.us.org>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/05 15:46:41 by sjones            #+#    #+#             */
-/*   Updated: 2017/06/15 18:34:08 by sjones           ###   ########.fr       */
+/*   Updated: 2017/06/15 18:37:09 by sjones           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,18 +20,17 @@ void	add_ent(t_db *db, char *line)
 	ft_putendl("plerp1");
 	if (DE == NULL)
     {
-	ft_putendl("plerp2");
         begin_list(db, line);
         return ;
     }
     t1 = DE;
 	t2 = init_ent(line);
-	ft_putendl("plerp3");
 	while (t1 && ft_strcmp(t1->start_date, t2->start_date) < 0)
 	{
 	ft_putendl("plerp4");
 		t1 = t1->next;
 	}
 	t1->next = t2;
+	all_puts(t1, stdout);
 	ft_putendl("plerp5");
 }
