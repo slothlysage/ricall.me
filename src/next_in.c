0ccdb7da99b9eb1e96519a48ee11f578c6fe0537
Sean JONES <sjones@e1z2r5p43.42.us.org>
2017-06-15 20:23:38 -0700

    doop

--- src/next_in.c
+++ src/next_in.c
@@ -6,7 +6,7 @@
 /*   By: sjones <sjones@student.42.us.org>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/05 15:46:53 by sjones            #+#    #+#             */
-/*   Updated: 2017/06/15 20:19:36 by sjones           ###   ########.fr       */
+/*   Updated: 2017/06/15 20:23:30 by sjones           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ char    *next_in(char *line)
 {
     line = ft_strchr(line, 44);
     line = ft_strchr(line, 58);
-    line += (*(line + 2) == 39) ? (3) : (2);
+    line += 2;
+	if (*line == 39)
+		line++;
     return (line);
 }
