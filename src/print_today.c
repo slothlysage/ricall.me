59232612d7c946e65fc8eec8a41721cd5c4d150e
Sean JONES <sjones@e1z2r2p15.42.us.org>
2017-06-17 16:05:28 -0700

    deactivating print_today

--- src/print_today.c
+++ src/print_today.c
@@ -6,7 +6,7 @@
 /*   By: sjones <sjones@student.42.us.org>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/05 15:48:45 by sjones            #+#    #+#             */
-/*   Updated: 2017/06/17 16:03:58 by sjones           ###   ########.fr       */
+/*   Updated: 2017/06/17 16:05:14 by sjones           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,12 @@
 
 int		print_today(t_db *db)
 {
-	t_ent	*all;
+//	t_ent	*all;
 //	t_ent	*today;
 //	t_ent	*all_meds;
 //	t_ent	*today_meds;
 
-	all = DE;
+//	all = DE;
 //	all_meds = get_meds(all);
 //	today = get_today(all);
 //	today_meds = get_meds(today);
