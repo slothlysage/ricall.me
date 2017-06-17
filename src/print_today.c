faf63c6b4758d11e2f9c29b4af2afe59fdddd6f5
Sean JONES <sjones@e1z2r2p15.42.us.org>
2017-06-17 15:42:46 -0700

    input_debugging and today fix

--- src/print_today.c
+++ src/print_today.c
@@ -6,7 +6,7 @@
 /*   By: sjones <sjones@student.42.us.org>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/05 15:48:45 by sjones            #+#    #+#             */
-/*   Updated: 2017/06/17 15:39:00 by sjones           ###   ########.fr       */
+/*   Updated: 2017/06/17 15:42:11 by sjones           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,7 @@ t_ent	*get_meds(t_ent *t)
 	{
 		if (meds->categoryid == 1)
 		{
+			all_puts(meds, stdout);
 			while (meds->next)
 			{
 				if (meds->next->categoryid != 1)
@@ -45,6 +46,7 @@ t_ent	*get_today(t_ent *t)
 
 	ti = time(NULL);
 	tm = localtime(&ti);
+	date = "";
 	sprintf(date, "%d-%d-%d\n", tm->tm_year + 1900, tm->tm_mon + 1, tm->tm_mday);
 	while (t && ft_strncmp(t->start_date, date, 10) != 0)
 		t = t->next;
@@ -65,9 +67,7 @@ int		print_today(t_db *db)
 	t_ent	*t;
 	t_ent	*today;
 	t_ent	*meds;
-	FILE	*fp;
 
-	fp = stdout;
 	t = DE;
 	today = get_today(t);
 	meds = get_meds(today);
