c6b62f5e304ae3e0a02c2fa3a29934f9af75d176
Sean JONES <sjones@e1z2r2p15.42.us.org>
2017-06-18 13:35:16 -0700

    get_time fixes

--- src/get_today.c
+++ src/get_today.c
@@ -6,7 +6,7 @@
 /*   By: sjones <sjones@student.42.us.org>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/06/17 15:53:00 by sjones            #+#    #+#             */
-/*   Updated: 2017/06/18 13:26:14 by sjones           ###   ########.fr       */
+/*   Updated: 2017/06/18 13:34:56 by sjones           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,32 +25,26 @@ t_ent	*get_time(t_ent *t, int start, int end)
 {
 	t_ent		*today;
 	t_ent		*head;
-	char		date[2];
-	int			dn1;
-	int			dn2;
+	int			dn;
 
 	ft_putendl("entering get time");
 	today = NULL;
-	sprintf(date, "%02d", start);
-	dn1 = ft_atoi(date);
 	today = t;
-	dn2 = grab_hour(today->start_date);
-	while (today->next && dn2 < dn1)
+	dn = grab_hour(today->start_date);
+	while (today->next && dn < start)
 	{
 		today = today->next;
-		dn2 = grab_hour(today->start_date);
+		dn = grab_hour(today->start_date);
 	}
 	head = today;
-	all_puts(head, stdout);
-	sprintf(date, "%02d", end);
-	dn1 = ft_atoi(date);
-	while (today->next && dn2 < dn1)
+	while (today->next && dn < end)
 	{
 		today = today->next;
-		dn2 = grab_hour(today->start_date);
+		dn = grab_hour(today->start_date);
 	}
-	if (grab_hour(today->start_date) > dn1)
+	if (grab_hour(today->start_date) > end)
 		today = NULL;
+	all_puts(head, stdout);
 	ft_putendl("Survived get time");
 	return (head);
 }
@@ -78,7 +72,6 @@ t_ent	*get_today(t_ent *t)
 	ti = time(NULL);
 	tm = localtime(&ti);
 	sprintf(date, "%.4d%.2d%.2d", tm->tm_year + 1900, tm->tm_mon + 1, tm->tm_mday);
-	ft_putendl(date + 2);
 	dn1 = ft_atoi(date + 2);
 	today = t;
 	dn2 = date_grab(today->start_date);
