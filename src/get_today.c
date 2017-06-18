b0c1cffb99c9e0531c03e517b441759a36093608
Sean JONES <sjones@e1z2r2p15.42.us.org>
2017-06-18 00:11:24 -0700

    working on print_today

--- src/get_today.c
+++ src/get_today.c
@@ -6,7 +6,7 @@
 /*   By: sjones <sjones@student.42.us.org>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/06/17 15:53:00 by sjones            #+#    #+#             */
-/*   Updated: 2017/06/18 00:09:22 by sjones           ###   ########.fr       */
+/*   Updated: 2017/06/18 00:11:14 by sjones           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -62,7 +62,7 @@ t_ent	*get_today(t_ent *t)
 	ft_putendl(date);
 	dn = ft_atoi(date);
 	today = t;
-	all_puts(today, stdout);
+	all_puts(today->next, stdout);
 	while (today->next != NULL && dn > date_grab(today->start_date))
 	{
 		all_puts(today, stdout);
