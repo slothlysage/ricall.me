9671b0a1e2dc709880ade4e4e8588821b56b7d73
Sean JONES <sjones@e1z2r2p15.42.us.org>
2017-06-17 20:39:42 -0700

    working on print_today

--- src/get_today.c
+++ src/get_today.c
@@ -6,7 +6,7 @@
 /*   By: sjones <sjones@student.42.us.org>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/06/17 15:53:00 by sjones            #+#    #+#             */
-/*   Updated: 2017/06/17 20:37:39 by sjones           ###   ########.fr       */
+/*   Updated: 2017/06/17 20:39:37 by sjones           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,14 +24,14 @@ t_ent	*get_time(t_ent *t, int start, int end)
 	ti = time(NULL);
 	tm = localtime(&ti);
 	ft_putendl("get time 2");
-	sprintf(date, "%d-%d-%dT%2d\n", tm->tm_year + 1900, tm->tm_mon + 1, tm->tm_mday, start);
+	sprintf(date, "%d-%02d-%02dT%02d\n", tm->tm_year + 1900, tm->tm_mon + 1, tm->tm_mday, start);
 	ft_putendl(date);
 	while (t->next && ft_strncmp(t->start_date, date, 13) < 0)
 		t = t->next;
 	ft_putendl("get time 3");
 	today = t;
 	t = t->next;
-	sprintf(date, "%d-%d-%dT%2d\n", tm->tm_year + 1900, tm->tm_mon + 1, tm->tm_mday, end);
+	sprintf(date, "%d-%02d-%02dT%02d\n", tm->tm_year + 1900, tm->tm_mon + 1, tm->tm_mday, end);
 	ft_putendl(date);
 	ft_putendl("get time 4");
 	while (t->next && ft_strncmp(t->start_date, date, 13) < 0)
