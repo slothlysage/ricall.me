2d847f28341698891918180d7ca914c7354ff836
Sean JONES <sjones@e1z2r2p15.42.us.org>
2017-06-18 00:00:58 -0700

    working on print_today

--- src/get_today.c
+++ src/get_today.c
@@ -6,7 +6,7 @@
 /*   By: sjones <sjones@student.42.us.org>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/06/17 15:53:00 by sjones            #+#    #+#             */
-/*   Updated: 2017/06/18 00:00:20 by sjones           ###   ########.fr       */
+/*   Updated: 2017/06/18 00:00:53 by sjones           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,7 +40,7 @@ int		date_grab(char *time)
 {
 	char	tmp[8];
 
-	sprintf(tmp, "%.4s%.2s%.2s", time, time + 5, time + 1);
+	sprintf(tmp, "%.4s%.2s%.2s", time, time + 5, time + 2);
 	ft_putendl(tmp);
 	fputs(tmp, stdout);
 	return(ft_atoi(tmp));
