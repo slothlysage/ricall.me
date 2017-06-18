6f72fc4734ae4b639eea4a94603c43cc7e3b7cb5
Sean JONES <sjones@e1z2r2p15.42.us.org>
2017-06-17 20:26:20 -0700

    working on print_today

--- src/get_today.c
+++ src/get_today.c
@@ -6,7 +6,7 @@
 /*   By: sjones <sjones@student.42.us.org>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/06/17 15:53:00 by sjones            #+#    #+#             */
-/*   Updated: 2017/06/17 19:31:46 by sjones           ###   ########.fr       */
+/*   Updated: 2017/06/17 20:26:15 by sjones           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,16 @@ t_ent	*get_time(t_ent *t, int start, int end)
 	time_t		ti; 
 	struct tm	*tm;
 
+	ft_putendl("begin get time");
+	today = NULL;
 	ti = time(NULL);
 	tm = localtime(&ti);
 	date = "";
+	ft_putendl("get time 2");
 	sprintf(date, "%d-%d-%dT%2d\n", tm->tm_year + 1900, tm->tm_mon + 1, tm->tm_mday, start);
 	while (t && ft_strncmp(t->start_date, date, 13) < 0)
 		t = t->next;
+	ft_putendl("get time 3");
 	today = t;
 	t = t->next;
 	sprintf(date, "%d-%d-%dT%2d\n", tm->tm_year + 1900, tm->tm_mon + 1, tm->tm_mday, end);
@@ -35,5 +39,6 @@ t_ent	*get_time(t_ent *t, int start, int end)
 		today->next = t;
 		t = t->next;
 	}
+	ft_putendl("survived getting time");
 	return (today);
 }
