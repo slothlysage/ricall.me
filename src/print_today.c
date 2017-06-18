0686e44f9d7ff536100f2a76299bafc738d50af7
Sean JONES <sjones@e1z2r2p15.42.us.org>
2017-06-17 19:07:20 -0700

    working on print_today

--- src/print_today.c
+++ src/print_today.c
@@ -6,7 +6,7 @@
 /*   By: sjones <sjones@student.42.us.org>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/05 15:48:45 by sjones            #+#    #+#             */
-/*   Updated: 2017/06/17 19:02:14 by sjones           ###   ########.fr       */
+/*   Updated: 2017/06/17 19:07:10 by sjones           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,14 @@ int		print_today(t_db *db)
 	t_ent	*all_meds;
 	t_ent	*today_meds;
 
-	all = NULL;
-	today = NULL;
-	all_meds = NULL;
-	today_meds = NULL;
 	all = DE;
 	all_meds = get_meds(all);
 	today = get_today(all);
 	today_meds = get_meds(today);
 //	all_puts(DE, stdout);
+	(void) all;
+	(void) today;
+	(void) all_meds;
+	(void) today_meds;
 	return (1);
 }
