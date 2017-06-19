e29cc2cf4cd82408f27d3e1e297e03509bf0c5f4
Sean JONES <sjones@e1z2r2p15.42.us.org>
2017-06-18 21:52:59 -0700

    time fix

--- src/add_ent.c
+++ src/add_ent.c
@@ -6,7 +6,7 @@
 /*   By: sjones <sjones@student.42.us.org>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/05 15:46:41 by sjones            #+#    #+#             */
-/*   Updated: 2017/06/18 21:34:13 by sjones           ###   ########.fr       */
+/*   Updated: 2017/06/18 21:52:54 by sjones           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,6 +43,7 @@ void	add_ent(t_db *db, char *line)
 	while (t1->next != NULL && e1 > e2)
 	{
 		t1 = t1->next;
+		e2 = get_mktime(t2->start_date);
 	}
 	if (ft_strcmp(t1->event_id, t2->event_id) != 0)
 	{
