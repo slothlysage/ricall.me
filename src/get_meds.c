733f99c584c8c74f017f35993e93bb20635130e9
Sean JONES <sjones@e1z2r2p15.42.us.org>
2017-06-18 20:31:54 -0700

    more get_meds work

--- src/get_meds.c
+++ src/get_meds.c
@@ -6,7 +6,7 @@
 /*   By: sjones <sjones@student.42.us.org>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/06/17 15:54:55 by sjones            #+#    #+#             */
-/*   Updated: 2017/06/18 20:27:49 by sjones           ###   ########.fr       */
+/*   Updated: 2017/06/18 20:31:34 by sjones           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,7 +42,7 @@ t_ent	*get_meds(t_ent *t)
 		ft_putendl("get_meds");
 	}
 	ft_putendl("survived looping get_meds");
-	if (meds->categoryid != 1)
+	if (meds->next != NULL &&  meds->categoryid != 1)
 		meds = NULL;
 	ft_putendl("survived getting meds");
 	return (head);
