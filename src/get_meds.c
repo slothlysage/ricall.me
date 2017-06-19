af09d0cad985f419fed73eb5676e0a84929cbd6e
Sean JONES <sjones@e1z2r2p15.42.us.org>
2017-06-18 20:41:29 -0700

    better

--- src/get_meds.c
+++ src/get_meds.c
@@ -6,7 +6,7 @@
 /*   By: sjones <sjones@student.42.us.org>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/06/17 15:54:55 by sjones            #+#    #+#             */
-/*   Updated: 2017/06/18 20:38:16 by sjones           ###   ########.fr       */
+/*   Updated: 2017/06/18 20:41:19 by sjones           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,7 @@ t_ent	*get_meds(t_ent *t)
 	ft_putendl("get_meds2");
 	meds = t;
 	head = meds;
+	all_puts(head, stdout);
 	ft_putendl("get_meds3");
 	while (meds && meds->categoryid == 1)
 	{
